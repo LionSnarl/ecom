f695060e35e07333c95f763b4bd94a9338e6c532
Peter Sarll <petersarll@gmail.com>
2020-04-07 15:19:31 -0700

    creating homepage component

---
+++ ecommerce/src/Homepage.component.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item>">
+        <div className="content"></div>
+      </div>
+    </div>
+  </div>
+)
