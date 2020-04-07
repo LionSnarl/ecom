7acc40bb42d5a30d62fd5ed7c8a03595b02a51c6
Peter Sarll <petersarll@gmail.com>
2020-04-07 15:22:01 -0700

    creating different divs inside of homepage directory

--- ecommerce/src/Homepage.component.jsx
+++ ecommerce/src/Homepage.component.jsx
@@ -4,7 +4,32 @@ const HomePage = () => (
   <div className="homepage">
     <div className="directory-menu">
       <div className="menu-item>">
-        <div className="content"></div>
+        <div className="content">
+          <h1 className="title">
+            <span className="subtitle"></span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item>">
+        <div className="content">
+          <h1 className="title">
+            <span className="subtitle"></span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item>">
+        <div className="content">
+          <h1 className="title">
+            <span className="subtitle"></span>
+          </h1>
+        </div>
+      </div>
+      <div className="menu-item>">
+        <div className="content">
+          <h1 className="title">
+            <span className="subtitle"></span>
+          </h1>
+        </div>
       </div>
     </div>
   </div>
