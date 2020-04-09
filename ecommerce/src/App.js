b69c26ddd9f8cf2756e4743331ee3a98cf9142ce
Peter Sarll <petersarll@gmail.com>
2020-04-09 13:23:01 -0700

    adding react router dom and rearranging exact path to mirror components

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import HomePage from './pages/HomePage/HomePage.component'
 import './App.css'
+import { Route } from 'react-router-dom'
+
+const hatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Route exact path="/" component={HomePage} />
     </div>
   )
 }
