07aff9b6557ff4dc940a278c3b0a6c71f3d7a5fd
Peter Sarll <petersarll@gmail.com>
2020-04-10 14:39:24 -0700

    adding filtering method to preview collection, adding stylization to individual items

--- ecommerce/src/components/preview-collection/preview-collection.component.jsx
+++ ecommerce/src/components/preview-collection/preview-collection.component.jsx
@@ -4,11 +4,13 @@ import './preview-collection.styles.scss'
 
 const PreviewCollection = ({ title, items, id }) => (
   <div className="collection-preview">
-    <h1>{title.toUpperCase()}</h1>
+    <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map((item) => (
-        <div key={item.id}>{item.name}</div>
-      ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
     </div>
   </div>
 )
