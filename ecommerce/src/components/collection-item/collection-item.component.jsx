55bd8fb3e2e870bb7b2ef62f310d34e4c57ec22d
Peter Sarll <petersarll@gmail.com>
2020-04-11 15:25:28 -0700

    changed classname to className

--- ecommerce/src/components/collection-item/collection-item.component.jsx
+++ ecommerce/src/components/collection-item/collection-item.component.jsx
@@ -6,8 +6,8 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
-      <span classname="name">{name}</span>
-      <span classname="price">{price}</span>
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
   </div>
 )
