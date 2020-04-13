f45fbe29d3b4711dc21195a4933ce6e2c400d3e9
Peter Sarll <petersarll@gmail.com>
2020-04-13 08:23:07 -0700

    adding signin/signout authentication to sign in with google

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,29 +1,45 @@
 import React from 'react'
 import './App.css'
 import { Switch, Route } from 'react-router-dom'
+import { auth } from './firebase/firebase.utils'
 
 import HomePage from './pages/HomePage/HomePage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignOut from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx'
 
-const hatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUser: null,
+    }
+  }
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignOut} />
-      </Switch>
-    </div>
-  )
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user })
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/signin" component={SignInAndSignOut} />
+        </Switch>
+      </div>
+    )
+  }
 }
-
 export default App
