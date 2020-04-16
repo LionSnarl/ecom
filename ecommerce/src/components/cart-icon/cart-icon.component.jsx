c7442ff7e61accf0a352f5759d7ff9b461326dc7
Peter Sarll <petersarll@gmail.com>
2020-04-16 16:08:12 -0700

    adding toggle function that allows cart component to hide or show

--- ecommerce/src/components/cart-icon/cart-icon.component.jsx
+++ ecommerce/src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './cart-icon.styles.scss'
+import { connect } from 'react-redux'
 
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from '../../pictures/cart-icon.svg'
 
-const CartIcon = () => (
-  <div className="cart-icon">
+const CartIcon = ({ toggleCartHidden }) => (
+  <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
     <span className="item-count"></span>
   </div>
 )
-export default CartIcon
+
+const mapDispatchToProps = (dispatch) => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden()),
+})
+export default connect(null, mapDispatchToProps)(CartIcon)
--- ecommerce/src/components/header/header.component.jsx
+++ ecommerce/src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import './header.styles.scss'
 
 import { auth } from '../../firebase/firebase.utils'
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -34,12 +34,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
-    <CartDropdown />
+    {hidden ? null : <CartDropdown />}
   </div>
 )
-
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+//state.user.currentUser === {user: {curentuser}}
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 })
 
 export default connect(mapStateToProps)(Header)
