14e3082c8771c24891ceed030e95201a99c20acf
Peter Sarll <petersarll@gmail.com>
2020-04-19 00:59:29 -0700

    adding reselector to cart-dropdown

--- ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ ecommerce/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import './cart-dropdown.styles.scss'
+import { createStructuredSelector } from 'reselect'
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 import CartItem from '../cart-item/cart-item.component'
 import CustomButton from '../custom-button/custom-button.component'
@@ -16,7 +18,7 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = createStructuredSelector({
+  cartItems: selectCartItems,
 })
 export default connect(mapStateToProps)(CartDropdown)
