79544c1f2cf196971dd19cefa9d37e424e4a8bca
Peter Sarll <petersarll@gmail.com>
2020-04-19 01:02:21 -0700

    in cart-icon component, refactoring and adding createStructuredSelector to cart-icon

--- ecommerce/src/components/cart-icon/cart-icon.component.jsx
+++ ecommerce/src/components/cart-icon/cart-icon.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './cart-icon.styles.scss'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
@@ -19,8 +20,8 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
 
-const mapStateToProps = (state) => ({
-  itemCount: selectCartItemsCount(state),
+const mapStateToProps = createStructuredSelector({
+  itemCount: selectCartItemsCount,
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
