bf4d7d9d0612b865d602ba8e77f762cf7ce57685
Peter Sarll <petersarll@gmail.com>
2020-04-18 23:06:10 -0700

    adding cart icon component

--- ecommerce/src/components/cart-icon/cart-icon.component.jsx
+++ ecommerce/src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,24 @@ import { connect } from 'react-redux'
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
 import { ReactComponent as ShoppingIcon } from '../../pictures/cart-icon.svg'
+import CartItem from '../cart-item/cart-item.component'
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count"></span>
+    <span className="item-count">{itemCount}</span>
   </div>
 )
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
