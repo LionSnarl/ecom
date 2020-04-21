33416921bf7d6e7441132fb1c45f0f6096c4f09f
Peter Sarll <petersarll@gmail.com>
2020-04-20 21:12:57 -0700

    adding arrows to checkout

--- ecommerce/src/components/checkout-item/checkout-item.component.jsx
+++ ecommerce/src/components/checkout-item/checkout-item.component.jsx
@@ -12,7 +12,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name} </span>
+      <div className="arrow">&#10094;</div>
       <span className="quantity">{quantity} </span>
+      <div className="arrow">&#10095;</div>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
         &#10005;
