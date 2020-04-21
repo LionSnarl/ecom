225058bf2cc600091128630c58a35f3e0fa9330e
Peter Sarll <petersarll@gmail.com>
2020-04-20 21:52:54 -0700

    implementing the remove itenm compinent on redux

--- ecommerce/src/redux/cart/cart.reducer.js
+++ ecommerce/src/redux/cart/cart.reducer.js
@@ -18,6 +18,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       }
+
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      }
+
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
