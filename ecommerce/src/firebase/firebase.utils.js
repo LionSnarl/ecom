22f3ef3d2e6c86b60b6a9ebcd89e5304040db4f9
Peter Sarll <petersarll@gmail.com>
2020-04-22 15:08:14 -0700

    adding local storage

--- ecommerce/src/firebase/firebase.utils.js
+++ ecommerce/src/firebase/firebase.utils.js
@@ -44,7 +44,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef
 }
-
+console.log(localStorage)
 firebase.initializeApp(config)
 
 export const auth = firebase.auth()
